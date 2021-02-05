12490b1ca707bc839a5297c0ae86df34deebac91
GSweets221 <74419926+GSweets221@users.noreply.github.com>
2021-02-05 14:14:21 +0100

    Siqi git conection test

--- FerretMVC1/src/main/java/org/ecn/PAPPL6_2020/Model/UpdateM.java
+++ FerretMVC1/src/main/java/org/ecn/PAPPL6_2020/Model/UpdateM.java
@@ -8,7 +8,7 @@ import javax.swing.SwingWorker;
 
 /**
  * Classe correspondant aux mises à jour.
- * @Authors: Mathieu JUNG-MULLER & Bozhou WANG
+ * @Authors: Mathieu JUNG-MULLER & Bozhou WANG & Siqi Yu
 */
 
 public class UpdateM extends SwingWorker<Boolean,Object>{
