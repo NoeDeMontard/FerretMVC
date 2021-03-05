dd947605c89f858b5f9c049ec4078c3e2ed4ebb8
Chenkai-ZHANG <73237971+Chenkai-ZHANG@users.noreply.github.com>
2021-03-05 18:02:02 +0100

    modification RunGUI

--- FerretMVC1/src/main/java/org/ecn/Ferret/View/RunGUI.java
+++ FerretMVC1/src/main/java/org/ecn/Ferret/View/RunGUI.java
@@ -7,8 +7,10 @@ package org.ecn.Ferret.View;
 // import classes
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import org.ecn.Ferret.Controller.RunCTRL;
 
 /**
  * Classe d'affichage du lancement de l'application Ferret.
@@ -17,17 +19,29 @@ import javax.swing.*;
 public class RunGUI extends GUI{
     JPanel goPanel = new JPanel();
     JButton goButton = new JButton("Run Ferret, Run!");
-    
-    public void addRun(GUI g){
+    private PopulationGUI pg;
+    private RegionInteretGUI rig;
+    private MenuFerretGUI mfg;
+    public void addRun(GUI g, PopulationGUI pg, RegionInteretGUI rig, MenuFerretGUI mfg){
         g.bigPanel.add(goPanel);
         goPanel.add(goButton);
         goButton.setPreferredSize(new Dimension(320, 60));
         goPanel.setBackground(Color.gray);
         // setActionCommand
         goButton.setActionCommand("goButton");
+        this.rig=rig;
+        this.pg=pg;
+        this.mfg=mfg;
     }
     
     public void RunListener(ActionListener a) {
         goButton.addActionListener(a);
     }
+     public void actionPerformed(ActionEvent e){//appeler controller et transmettre les classes gui popultaion, region et settings pour verifier les parametres et faire l'execution 
+         RunCTRL runctrl=new RunCTRL(pg,rig,mfg.settings);
+         if (runctrl.VerificationParam()){
+             runctrl.ExecutionTraitement1KG();
+         }
+     }
+
 }
